951f37137af7283266958183e435323d337fa287
Mykola Previr <strm.previr@gmail.com>
2014-11-15 19:01:14 +0200

    Add dummy populate() method to test SQL db creation.

--- korean-grammar/app/src/main/java/kpi/mobcomp/korean_grammar/activities/ShowEntry.java
+++ korean-grammar/app/src/main/java/kpi/mobcomp/korean_grammar/activities/ShowEntry.java
@@ -3,7 +3,9 @@ package kpi.mobcomp.korean_grammar.activities;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,11 +14,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.TextView;
+import kpi.mobcomp.korean_grammar.storage.DBhandler;
 
 import kpi.mobcomp.korean_grammar.R;
+import kpi.mobcomp.korean_grammar.storage.GrammarEntrySchema;
 
 
 public class ShowEntry extends Activity {
+
+    /** SQL test method. Do not use */
+    public void populate(DBhandler iDBhandler) {
+        SQLiteDatabase db = iDBhandler.getWritableDatabase();
+
+        db.execSQL( "insert into " + GrammarEntrySchema.GrammarEntryStructure.TABLE_NAME + "("
+                + GrammarEntrySchema.GrammarEntryStructure.ENTRY_ID + ","
+                + GrammarEntrySchema.GrammarEntryStructure.TITLE + ","
+                + GrammarEntrySchema.GrammarEntryStructure.ENTRY_PAYLOAD + ")"
+
+                + " VALUES ("
+                + R.id.grammar_symnida + ","
+                + "'" + getString(R.string.grammar_symnida) + "'" + ","
+                + "'" + getString(R.string.grammar_symnida_text) + "')");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +45,8 @@ public class ShowEntry extends Activity {
         TextView textView = (TextView) findViewById(R.id.show_entry_text);
         Bundle extras = getIntent().getExtras();
         textView.setText(extras.getString("text"));
-
+        DBhandler database = new DBhandler(this.getBaseContext());
+//        populate(database);
     }
 
 
